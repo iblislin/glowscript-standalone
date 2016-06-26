0b708c56c98e500443f70af69c0e3e8218046208
Iblis Lin <iblis@hs.ntnu.edu.tw>
2016-06-26 16:13:35 +0800

    purge trailing space

--- ball-vpy/py/ball.py
+++ ball-vpy/py/ball.py
@@ -8,18 +8,18 @@ dt = 0.003
 def balljump(spd):
     ball = sphere(pos=vector(-15.0, 5.0, 0.0), radius=size, color=color.white)
     ball.velocity = vector(spd, -1.0, 0.0)
-    
+
     def jump():
         rate(1000, jump)
-    
+
         previous_x = ball.pos.x
         ball.pos = ball.pos + ball.velocity * dt
-    
+
         if ball.pos.y < size and ball.velocity.y < 0:
             ball.velocity.y = - ball.velocity.y
         else:
             ball.velocity.y = ball.velocity.y - g * dt
-    
+
         if ball.pos.x >= 15.0:
             ball.visible = False
     jump()
